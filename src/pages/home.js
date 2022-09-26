5845ecc84ad877ebbf10a6cb90dd551dd395c0df
David Jay <davidgljay@gmail.com>
2022-09-26 15:26:22 -0400

    Trying new path route...

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,10 @@
 const Home = () => {
-  return <div>
+  return <div style="padding: 20px">
     <h1>Embeddable Relationality Visualizations</h1>
       <p><a href="./reldefinition">/reldefinition</a></p>
       <p><a href="./limitstoprediction">/limitstoprediction</a></p>
-      <p><a href="./relgraph"></a>relgraph</p>
-      <p><a href="./relstructure" >relstructure</a></p>
+      <p><a href="./relgraph">/relgraph</a></p>
+      <p><a href="./relstructure" >/relstructure</a></p>
   </div>;
 };
 
