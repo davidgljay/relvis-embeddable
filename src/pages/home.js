f3b5cb83a5032655067d59f083087af0a6db8d8f
David Jay <davidgljay@gmail.com>
2022-09-26 14:06:21 -0400

    Adding links to home.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,9 @@ const Home = () => {
     <h1>Embeddable Relationality Visualizations</h1>
     <ul>
       <li><a href="./reldefinition">/reldefinition</a></li>
+      <li><a href="/limitstoprediction"></a></li>
+      <li><a href="/relgraph"></a></li>
+      <li><a href="/relstructure" ></a></li>
     </ul>
   </div>;
 };
