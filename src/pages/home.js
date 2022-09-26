f06a8054528133a8ed98bcd855f415fdd9766e3e
David Jay <davidgljay@gmail.com>
2022-09-26 17:12:40 -0400

    Adding proper routes in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,10 @@
 const Home = () => {
   return <div style={{padding: 20}}>
     <h1>Embeddable Relationality Visualizations</h1>
-      <p><a href="/?vis=reldefinition">/?vis=reldefinition</a></p>
-      <p><a href="/?vis=limitstoprediction">/?vis=limitstoprediction</a></p>
-      <p><a href="/?vis=relgraph">/?vis=relgraph</a></p>
-      <p><a href="/?vis=relstructure" >/?vis=relstructure</a></p>
+      <p><a href="./?vis=reldefinition">/?vis=reldefinition</a></p>
+      <p><a href="./?vis=limitstoprediction">/?vis=limitstoprediction</a></p>
+      <p><a href="./?vis=relgraph">/?vis=relgraph</a></p>
+      <p><a href="./?vis=relstructure" >/?vis=relstructure</a></p>
   </div>;
 };
 
